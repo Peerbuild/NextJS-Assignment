6a0064d61b8bd7396e03c31fe4ca96f83e5f667e
codewarnab <dp950810@gmail.com>
2024-09-14 15:29:03 +0530

    fix

--- src/components/customInput.tsx
+++ src/components/customInput.tsx
@@ -40,7 +40,7 @@ export function EmailSubscriptionForm() {
             variant: 'subscribed',
             icon: (
                 <Image
-                    src="/checkcircle.svg"
+                    src="checkCircle.svg"
                     alt="Check"
                     width={20}  
                     height={20}
