a244ecc8e25c493d6813857ed66d97b963260486
codewarnab <dp950810@gmail.com>
2024-09-17 09:42:50 +0530

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,4 +4,4 @@ export const API_AUTH_PREFIX = "/api/auth";
 
 export const AUTH_ROUTES = ["/admin/login"];
 
-export const PROTECTED_ROUTES = [];
+export const PROTECTED_ROUTES = ["/admin/header", "/admin/emails"];
